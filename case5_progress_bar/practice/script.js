6145a3d0624b31eabf78b93dddf842e7ba6a50d0
suzy <suzyp0223@gmail.com>
2024-12-17 13:46:38 +0900

    프로그래스바 주석추가

--- case5_progress_bar/practice/script.js
+++ case5_progress_bar/practice/script.js
@@ -1,4 +1,4 @@
-;(function () {
+; (function () {
   'use strict'
 
   const get = (target) => {
@@ -7,13 +7,16 @@
 
   let timerId;
 
+  // throttle: 일정한 주기마다 이벤트를 발생시키는 방법
+  // 예시) 스크롤 이벤트
   const throttle = (callback, time) => {
     if (timerId) return;
+    // 한번 실행 후 초기화 하는 코드
     timerId = setTimeout(() => {
       callback();
       timerId = undefined;
     }, time);
-   };
+  };
 
   const $progressBar = get('.progress-bar');
 
@@ -22,6 +25,7 @@
       document.documentElement.scrollHeight -
       document.documentElement.clientHeight;
 
+    // 스크롤 높이
     const scrollTop = document.documentElement.scrollTop;
     const width = (scrollTop / height) * 100
     $progressBar.style.width = width + '%';
