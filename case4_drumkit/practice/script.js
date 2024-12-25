550cb1be3d6d467365bc0a583534866b7fcd428d
MyABi <mac@macs-MacBook-Pro.local>
2024-12-25 14:45:43 +0900

    04

--- case4_drumkit/practice/script.js
+++ case4_drumkit/practice/script.js
@@ -9,6 +9,10 @@
     return document.querySelectorAll(target)
   }
 
+  // const keys = getAll('.key');
+  const keys = Array.from(getAll('.key'));
+  // console.log(keys);
+
   const soundsRoot = 'assets/sounds/'
   const drumSounds = [
     { key: 81, sound: 'clap.wav' },
@@ -21,4 +25,54 @@
     { key: 88, sound: 'snare.wav' },
     { key: 67, sound: 'tom.wav' },
   ]
+
+  const getAudioElement = (index) => {
+    const audio = document.createElement('audio');
+    audio.dataset.key = drumSounds[index].key
+    audio.src = soundsRoot + drumSounds[index].sound;
+    return audio;
+  };
+
+  const playSound = (keycode) => {
+    const $audio = get(`audio[data-key="${keycode}"]`);
+    const $key = get(`div[data-key="${keycode}"]`);
+    // console.log("$key:",$key)
+    if ($audio && $key) {
+      $key.classList.add('playing');
+      $audio.currentTime = 0;
+      $audio.play();
+    };
+  };
+  const onKeyDown = (e) => {
+    // console.log("e.keycode: ",e.keyCode);
+    playSound(e.keyCode);
+  };
+
+  const onMouseDown = (e) => {
+    const keycode = e.target.getAttribute('data-key');
+    // console.log("keycode", keycode);
+    playSound(keycode);
+  };
+
+  const onTransitionEnd = (e) => {
+    if (e.propertyName === 'transform') {
+      e.target.classList.remove('playing');
+    }
+  };
+
+  const init = () => {
+    window.addEventListener('keydown', onKeyDown);
+    keys.forEach((key, index) => {
+      const audio = getAudioElement(index);
+      key.appendChild(audio);
+      key.dataset.key = drumSounds[index].key;
+      key.addEventListener('click', onMouseDown);
+      key.addEventListener('transitionend', onTransitionEnd);
+
+    });
+  };
+
+  init();
+
+
 })()
