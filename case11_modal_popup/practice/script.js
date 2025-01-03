2f7c12c2a3c4e3789ebf8eed6e15460b0506d398
suzy <suzyp0223@gmail.com>
2025-01-03 22:06:59 +0900

    모달 배경 클릭시 꺼짐

--- case11_modal_popup/practice/script.js
+++ case11_modal_popup/practice/script.js
@@ -1,6 +1,38 @@
-;(function () {
+; (function () {
   'use strict'
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $button = get('.modal_open_button');
+  const $modal = get('.modal');
+  const $body = get('body');
+  const $modalConfirmButton = get('.modal_button.confirm');
+  const $modalCancelButton = get('.modal_button.cancel');
+
+  const toggleModal = () => {
+    $modal.classList.toggle('show');
+    $body.classList.toggle('scroll_lock');
+  };
+
+
+  $button.addEventListener('click', () => {
+    toggleModal();
+  });
+
+  $modalConfirmButton.addEventListener('click', () => {
+    toggleModal();
+  });
+
+  $modalCancelButton.addEventListener('click', () => {
+    toggleModal();
+  });
+
+  // 배경클릭시 모달 꺼짐.
+  window.addEventListener('click', (e) => {
+    if (e.target === $modal) {
+      toggleModal();
+    }
+  });
+
 })()
