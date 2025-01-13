9cdf5bdae2210ddb4c87f71408ec7af50ad25188
suzy <suzyp0223@gmail.com>
2025-01-13 18:57:40 +0900

    3D  carousel

--- case13_3d_carousel/practice/script.js
+++ case13_3d_carousel/practice/script.js
@@ -1,7 +1,28 @@
-;(function () {
+; (function () {
   'use strict'
 
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const cellCount = 6;
+  let selectIndex = 0;  // 초기값
+  const prevButton = get('.prev_button');
+  const nextButton = get('.next_button');
+  const carousel = get('.carousel');
+
+  const rotateCarousel = () => {
+    const angle = (selectIndex / cellCount) * -360;
+    carousel.style.transform = `translateZ(-346px) rotateY(${angle}deg)`;
+  };
+
+  prevButton.addEventListener('click', () => {
+    selectIndex--;
+    rotateCarousel();
+  });
+  nextButton.addEventListener('click', () => {
+    selectIndex++;
+    rotateCarousel();
+  });
+
 })()
